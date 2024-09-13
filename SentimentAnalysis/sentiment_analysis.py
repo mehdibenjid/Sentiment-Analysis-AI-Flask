c9b3a78389e9a6a9df9d7ea24d185f748a4c52e7
mehdibenjid <mehdibenjid01@gmail.com>
2024-09-13 16:37:46 +0200

    static code analysis : 10/10 on sentiment_analysis.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,9 +1,24 @@
-import requests
+"""
+    This module provides sentiment analysis functionality using an external API.
+"""
 import json
-
+import requests
 def sentiment_analyzer(text_to_analyse):
+    """
+    Sends text to an external API for sentiment analysis and returns the sentiment label and score.
+
+    Args:
+        text_to_analyse (str): The text to be analyzed.
+
+    Returns:
+        dict: A dictionary containing 'label' and 'score' keys for the sentiment analysis result.
+    """
+
     # URL of the sentiment analysis service
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    url = (
+        'https://sn-watson-sentiment-bert.labs.skills.network/v1/'
+        'watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    )
 
     # Constructing the request payload in the expected format
     myobj = { "raw_document": { "text": text_to_analyse } }
@@ -12,16 +27,19 @@ def sentiment_analyzer(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
 
     # Sending a POST request to the sentiment analysis API
-    response = requests.post(url, json=myobj, headers=header)
+    response = requests.post(url, json=myobj, headers=header, timeout=10)
+
 
     # Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
 
+    label = None
+    score = None
     # Extracting sentiment label and score from the response
     if response.status_code == 200 :
         label = formatted_response['documentSentiment']['label']
         score = formatted_response['documentSentiment']['score']
-    elif response.status_code == 500 : 
+    elif response.status_code == 500 :
         label = None
         score = None
 
