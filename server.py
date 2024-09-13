9120d5ce7be03f2abde82e5298ebdf7083c58c97
mehdibenjid <mehdibenjid01@gmail.com>
2024-09-13 16:22:14 +0200

    error handling added

--- server.py
+++ server.py
@@ -11,17 +11,22 @@ from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 app = Flask("Sentiment Analyzer")
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
-    ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
-        function. The output returned shows the label and its confidence 
-        score for the provided text.
-    '''
+    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
-    response = sentiment_analyzer(text_to_analyze)
-    score = response['score']
-    label = response['label'].split('_')[1]
-    return f"The given text has been identified as {label} with a score of {score} "
 
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = sentiment_analyzer(text_to_analyze)
+
+    # Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+
+    # Check if the label is None, indicating an error or invalid input
+    if label is None:
+        return "Invalid input! Try again."
+    else:
+        # Return a formatted string with the sentiment label and score
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
@@ -32,4 +37,4 @@ def render_index_page():
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
